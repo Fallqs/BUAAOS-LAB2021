95a99e61196c916cea785dda074d0b6644a3be19
19375273 <jfsilentspring@buaa.edu.cn>
2021-03-09 16:28:31 +0800

    op5

--- dst/palindrome.c
+++ dst/palindrome.c
@@ -5,7 +5,7 @@ int main()
 	scanf("%d",&n);
 	int a[32],tp=0;
 	while(n)a[tp++]=n%10,n/=10;
-	int i=0;for(;i<(tp>>1);++i)if(a[i]!=a[tp-i]){
+	int i=0;for(;i<=(tp>>1);++i)if(a[i]!=a[tp-i-1]){
 		printf("N");return 0;
 	}
 	printf("Y");
