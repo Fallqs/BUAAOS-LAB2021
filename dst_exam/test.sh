12e9d077c2b629c0fa0cf72f24b754d509b9ec63
10.210.0.67 <10.210.0.67@example.com>
2021-03-16 15:22:37 +0800

    Initial lab0-Extra

--- dst_exam/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-grep -r -n  -e 'hello OS lab0' dir >lab0_exam.c
-find -name  lab0_x -printf "%P\n">>lab0_exam.c
--- exercise/shsh/shh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-a=1
-while [ $a -ne 10 ]
-	do
-		echo $a
-		a=$[$a+1]
-	done
-chmod +x shh.sh
