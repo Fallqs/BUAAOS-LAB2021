93facf048d5eb3ddedfc00f2cb1bac886c18fd28
19375273 <jfsilentspring@buaa.edu.cn>
2021-04-27 14:45:14 +0800

    auto push

--- lib/sched.c
+++ lib/sched.c
@@ -12,6 +12,23 @@
  *  3. CANNOT use `return` statement!
  */
 /*** exercise 3.14 ***/
+int PRI(struct Env *e){return e->env_pri&255;}
+int FUNC1(struct Env *e){
+    return (e->env_pri>>8)&255;
+}
+void sPRI(struct Env *e, int x){
+    e->env_pri>>=8;
+    e->env_pri<<=8;
+    e->env_pri|=(x&255);
+}
+void UPD(struct Env *e){
+    if(e!=NULL){
+	int n = PRI(e) - FUNC1(e);
+//	printf("%x:%d\n",(u_long)e, n);
+	sPRI(e, n<0?0:n);
+    }
+}
+
 void sched_yield(void)
 {
     static int count = 0; // remaining time slices of current env
@@ -32,11 +49,13 @@ void sched_yield(void)
      *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
      */
     if(count<=0){
+	    UPD(e);
+
 	    do{
 		    if( LIST_EMPTY(&env_sched_list[point]) ) point = 1 - point;
 		    
 		    LIST_FOREACH(ei, &env_sched_list[point], env_sched_link){
-			if(e==NULL|| e->env_status != ENV_RUNNABLE || ei->env_pri > e->env_pri
+			if(e==NULL|| e->env_status != ENV_RUNNABLE || PRI(ei) > PRI(e)
 				&& ei->env_status== ENV_RUNNABLE) e = ei;
 		    }
 		    /*
