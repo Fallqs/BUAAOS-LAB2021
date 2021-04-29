6a28c7e3a0c11d3cdce981f41bfbdb7061c82539
19375273 <jfsilentspring@buaa.edu.cn>
2021-04-29 09:14:47 +0800

    auto push

--- lib/sched.c
+++ lib/sched.c
@@ -31,7 +31,7 @@ void sched_yield(void)
      *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
      */
 
-    if(count<=0 || e == NULL || e->env_status != ENV_RUNNABLE){
+    if(count<=0 || curenv == NULL || curenv->env_status != ENV_RUNNABLE){
         do{
             if( LIST_EMPTY(&env_sched_list[point]) ) point ^= 1;
 
@@ -44,11 +44,38 @@ void sched_yield(void)
             }
 
         }while(e==NULL || e->env_status != ENV_RUNNABLE);
+        
+        count = e->env_pri;
     }
     
-    count = e->env_pri;
 
 /*
+    if (count == 0 || curenv == NULL || e->env_status != ENV_RUNNABLE) {
+        if (curenv != NULL) {
+            if (curenv->env_status != ENV_FREE) {
+                LIST_INSERT_TAIL(&env_sched_list[1 - point], curenv, env_sched_link);
+            }
+        }
+
+        do {
+            if (LIST_EMPTY(&env_sched_list[point])) {
+                point = 1 - point;
+            }
+
+            e = LIST_FIRST(&env_sched_list[point]);
+
+            
+            LIST_REMOVE(e, env_sched_link);
+            if (e->env_status == ENV_NOT_RUNNABLE) {
+                LIST_INSERT_TAIL(&env_sched_list[1 - point], e, env_sched_link);
+            }
+        } while (e->env_status != ENV_RUNNABLE);
+
+      
+        count = e->env_pri;
+    }
+*/
+    /*
     if (count == 0 || curenv == NULL || e->env_status != ENV_RUNNABLE) {
         if (curenv != NULL) {
             if (curenv->env_status != ENV_FREE) {
@@ -75,7 +102,6 @@ void sched_yield(void)
         count = e->env_pri;
     }
 */
-
 
     count--;
     env_run(e);
