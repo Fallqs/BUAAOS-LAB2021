f17379768cbfc483c570517d66ea95708c0f0af3
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-08 22:47:17 +0800

    old raw2

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 const int N=64;
 int t[64];
-register int cnt=0;
+int cnt=0;
 inline void mx(int &x,int y){if(x>y)x=y;}
 inline void qry(long *pp,int rnk){
     int old=0;
-    for(int i=0;i<N;++i){
+    for(register int i=0;i<N;++i){
         if(pp[i]==rnk){t[i]=++cnt;return;}
         if(t[i]<t[old])old=i;
     }
-    pp[old]=rnk;t[old]=++cnt-10;
+    pp[old]=rnk;t[old]=++cnt-15;
 }
 void pageReplace(long * physic_memery, long nwAdd){
     qry(physic_memery, nwAdd>>12);
