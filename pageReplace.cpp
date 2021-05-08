ee98d738535de1e74d4ef4a54811ae82138d6c83
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-08 22:57:29 +0800

    old 3stg

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -7,10 +7,10 @@ inline void mx(int &x,int y){if(x>y)x=y;}
 inline void qry(long *pp,int rnk){
     register int old=0;
     for(register int i=0;i<N;++i){
-        if(pp[i]==rnk){t[i]=++cnt;return;}
+        if(pp[i]==rnk){t[i]=++cnt-((t[i]&1)<<2)<<1;return;}
         if(t[i]<t[old])old=i;
     }
-    pp[old]=rnk;t[old]=++cnt-25;
+    pp[old]=rnk;t[old]=++cnt-35<<1|1;
 }
 void pageReplace(long * physic_memery, long nwAdd){
     qry(physic_memery, nwAdd>>12);
