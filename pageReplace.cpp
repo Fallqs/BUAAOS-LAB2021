c2789b036875bbcb163b10c672afa85c808c11ce
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-08 22:31:24 +0800

    old2

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -1,12 +1,12 @@
 #include "pageReplace.h"
 using namespace std;
-const int N=64,I=8,E=3;
+const int N=64,I=64,E=3;
 int t[64];
 unsigned long long v;
 int cnt=0;
 inline void mx(int &x,int y){if(x>y)x=y;}
 inline void qry(long *pp,int rnk){
-    int ex=rnk&0x38,old=ex;
+    int ex=0,old=ex;
     for(int i=ex,j=ex+I;i<j;++i){
         if(t[i])t[i]=t[i]*3/4;
         if(pp[i]==rnk){mx(t[i]+=1<<25,1<<27);return;}
