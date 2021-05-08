65f958e7e8fe46da52910e25cee21f430c2a25fb
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-08 22:11:08 +0800

    link8

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -1,55 +1,17 @@
 #include "pageReplace.h"
 using namespace std;
-const int N=64;
-struct pg{
-    pg *l,*r;int rnk;
-};
-char id[1<<20];
-pg buf[N+1],*ll,*rr;
-const pg *lst=buf+1;
-inline void push(pg *p){
-    if(!rr)rr=p;
-    else{
-        rr->r=p;p->l=rr;rr=p;
+const int N=64,I=8,E=3;
+int r[64],t[64];
+unsigned long long v;
+int cnt=0;
+inline void qry(long *pp,int rnk){
+    int ex=rnk&0x38,old=ex;
+    for(int i=ex,j=ex+I;i<j;++i){
+        if(r[i]==rnk){t[i]=++cnt;return;}
+        if(t[i]<t[old])old=i;
     }
-    if(!ll)ll=p;
-}
-inline void pushbk(pg *p){
-    if(!ll)ll=p;
-    else{
-        ll->l=p;p->r=ll;ll=p;
-    }
-    if(!rr)rr=p;
-}
-inline pg* pop(){
-    return (ll=ll->r)->l;
-}
-inline pg* del(pg *p){
-    if(p->r)p->r->l=p->l;
-    if(p->l)p->l->r=p->r;
-    if(p==ll)ll=ll->r;
-    if(p==rr)rr=rr->l;
-    p->r=0;
-    return p;
-}
-int sz;
-inline void ins(long *pp,int r){
-    int ind=id[r];pg *p;
-    if(ind){
-        push(del(buf+ind));
-        return;
-    }else if(sz<N){
-        pp[sz]=r;
-        p=buf+(++sz);
-        push(p);
-    }else{
-        pp[ll-lst]=r;
-        id[ll->rnk]=0;
-        push(del(p=ll));
-    }
-    p->rnk=r;
-    id[r]=p-buf;
+    pp[old]=r[old]=rnk;t[old]=++cnt;
 }
 void pageReplace(long * physic_memery, long nwAdd){
-    ins(physic_memery, nwAdd>>12);
+    qry(physic_memery, nwAdd>>12);
 }
