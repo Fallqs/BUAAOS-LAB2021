c100c6afc4eb1229eb46183738982e64fd8f099f
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-09 13:25:24 +0800

    gui gui

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -3,12 +3,12 @@ using namespace std;
 const int N=64,M=256;
 int t[64],cnt=M;
 inline void qry(long *pp,int rnk){
-    register int old=0,j=++cnt;
+    register int old=0,j=++cnt,r=rnk;
     for(register int i=0;i<N;++i){
-        if(pp[i]==rnk){t[i]=j;return;}
+        if(pp[i]==r){t[i]=j;return;}
         if(t[i]<t[old])old=i;
     }
-    pp[old]=rnk;t[old]=j-M;
+    pp[old]=r;t[old]=j-M;
 }
 void pageReplace(long * physic_memery, long nwAdd){
     qry(physic_memery, nwAdd>>12);
