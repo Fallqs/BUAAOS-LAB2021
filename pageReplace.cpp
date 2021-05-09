851e55f666c6416467c5ce59ad8b3df3c3ef35fb
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-09 13:19:01 +0800

    gui gui

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -3,16 +3,10 @@ using namespace std;
 const int N=64;
 int t[64];
 int cnt=256;
-inline void upd(int &x,int j){
-    register int p=x&3;
-    register int y=x&2<<5+x&2<<4;
-    //register int z=x&1<<5;
-    x=(j)<<2|p>>1;
-}
 inline void qry(long *pp,int rnk){
     register int old=0,j=++cnt;
     for(register int i=0;i<N;++i){
-        if(pp[i]==rnk){upd(t[i],j);return;}
+        if(pp[i]==rnk){t[i]=j;return;}
         if(t[i]<t[old])old=i;
     }
     pp[old]=rnk;t[old]=j-256<<2|2;
