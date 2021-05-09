2b0b073dd1f918c2d60aef112bf4934b3c03c5b4
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-09 13:31:25 +0800

    gui gui rint

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -5,10 +5,10 @@ int t[64],cnt=M;
 inline void qry(register long *pp,register int rnk){
     register int old=0,j=++cnt;
     for(register int i=0;i<N;++i){
-        if(pp[i]==r){t[i]=j;return;}
+        if(pp[i]==rnk){t[i]=j;return;}
         if(t[i]<t[old])old=i;
     }
-    pp[old]=r;t[old]=j-M;
+    pp[old]=rnk;t[old]=j-M;
 }
 void pageReplace(long * physic_memery, long nwAdd){
     qry(physic_memery, nwAdd>>12);
