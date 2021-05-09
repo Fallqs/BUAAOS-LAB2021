4f8c9b546b4cfc74a0e8a50a5f2a7cb042b9a0ed
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-10 00:30:19 +0800

    auto

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -1,17 +1,16 @@
-//#pragma GCC optimize(2)
 #include "pageReplace.h"
 using namespace std;
-const int N=64,M=426;
+const int N=64,M=426,Q=N>>1;
 int t[64],cnt=M;
 inline void qry(register long *pp,register int rnk){
-    register int *old=t,j=++cnt;
-    register long *end=pp+N,*p=pp;
-    for(register int *i=t;p<end;++p,++i){
-        if(*p==rnk){*i=j;return;}
-        if(*i<*old)old=i;
-    }
-    *(pp+(old-t))=rnk;*old=j-M;
+	register int bs=rnk&Q,*old=t+bs,j=++cnt;
+	register long *end=pp+Q+bs,*p=pp+bs;
+	for(register int *i=old;p<end;++p,++i){
+		if(*p==rnk){*i=j;return;}
+		if(*i<*old)old=i;
+	}
+	*(pp+(old-t))=rnk;*old=j-M;
 }
 void pageReplace(long * physic_memery, long nwAdd){
-    qry(physic_memery, nwAdd>>12);
+	qry(physic_memery, nwAdd>>12);
 }
