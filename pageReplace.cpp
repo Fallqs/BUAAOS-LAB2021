5b3399a907e94158f155cc58f4009cb34df531e7
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-10 09:44:48 +0800

    l16-477-bit0

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -3,7 +3,7 @@ using namespace std;
 const int N=64,M=477,Q=16,R=48;
 int t[64],cnt=M;
 inline void qry(register long *pp,register int rnk){
-	register int bs=rnk&R,*old=t+bs,j=++cnt;
+	register int bs=(rnk&3)<<4,*old=t+bs,j=++cnt;
 	register long *p=pp+bs,*end=p+Q;
 	for(register int *i=old;p<end;++p,++i){
 		if(*p==rnk){*i=j;return;}
