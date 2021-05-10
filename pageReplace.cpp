32241c2ab2294f286412c0e1fd99c2e0cdf007ad
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-10 19:26:20 +0800

    auto

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -1,14 +1,14 @@
 #include "pageReplace.h"
 using namespace std;
-const int N=64,M=410,Q=16,R=48;
+const int N=64,M=410,Q=16;
 int t[64],cnt=M;
 inline void qry(register long *pp,register int rnk){
 	register int j=(rnk&3)<<4;
 	for(register long *p=pp+j,*end=p+Q;p<end;++p)
 		if(*p==rnk){*(t+(p-pp))=++cnt;return;}
-	register int *old=t+j;
+	register int *old=t+j;j=*old;
 	for(register int *i=old,*end=old+Q;i<end;++i)
-		if(*i<*old)old=i;
+		if(*i<j)j=*(old=i);
 	*(pp+(old-t))=rnk;*old=++cnt-M;
 }
 void pageReplace(long * physic_memery, long nwAdd){
