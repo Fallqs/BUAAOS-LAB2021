c6c51f734c9c3dcc25cb01c461d5c29b3c41de26
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-11 21:02:34 +0800

    ()

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -1,20 +1,20 @@
 #include "pageReplace.h"
 using namespace std;
 const int N=64,M=410,Q=16,R=48;
-int t[64],sz[64],cnt=M;
+int t[64],sz[4],cnt=M;
 void pageReplace(long *p,long nwAdd){
-	register int rnk=nwAdd>>12, j=(rnk&3)<<4;
-	if(sz[j]<Q){
-		register int end=j+(sz[j]++);
+	register int rnk(nwAdd>>12), i(rnk&3), j(i<<4);
+	if(sz[i]<Q){
+		register int end(j+sz[i]);
 		for(;j<end;++j)
 			if(p[j]==rnk){t[j]=++cnt;return;}
-		p[end]=rnk;t[end]=++cnt;
+		p[end]=rnk;t[end]=++cnt-M;++sz[i];
 		return;
 	}
-	register int old=j;
+	register int old(j);
 	for(register int end=j+Q;j<end;++j){
 		if(p[j]==rnk){t[j]=++cnt;return;}
 		if(t[j]<t[old])old=j;
 	}
-	p[old]=rnk;t[old]=++cnt;
+	p[old]=rnk;t[old]=++cnt-M;
 }
