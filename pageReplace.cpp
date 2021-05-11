99dd812ce18de4ee512e57d668f6a62725ff1ded
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-11 20:46:17 +0800

    auto

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -1,20 +1,16 @@
 #include "pageReplace.h"
 using namespace std;
 const int N=64,M=410,Q=16,R=48;
-int t[64],sz[64],cnt=M;
-void pageReplace(long *physic_memery,long nwAdd){
-	register int rnk=nwAdd>>12, j=(rnk&3)<<4;
-	if(sz[j]<Q){
-		register int end=j+sz[j], jj=j;
-		for(;j<end;++j)
-			if(physic_memery[j]==rnk){t[j]=++cnt;return;}
-		physic_memery[end]=rnk;t[end]=++cnt;++sz[jj];
-		return;
-	}
-	register int old=j;
-	for(register int end=j+Q;j<end;++j){
-		if(physic_memery[j]==rnk){t[j]=++cnt;return;}
-		if(t[j]<t[old])old=j;
-	}
-	physic_memery[old]=rnk;t[old]=++cnt;
+int t[64],cnt=M;
+inline void qry(register long *pp,register int rnk){
+	register int j=(rnk&3)<<4;
+	for(register long *p=pp+j,*end=p+Q;p<end;++p)
+		if(*p==rnk){*(t+(p-pp))=++cnt;return;}
+	register int *old=t+j;j=*old;
+	for(register int *i=old,*end=old+Q;i<end;++i)
+		if(*i<j)j=*(old=i);
+	*(pp+(old-t))=rnk;*old=++cnt-M;
+}
+void pageReplace(long * physic_memery, long nwAdd){
+	qry(physic_memery, nwAdd>>12);
 }
