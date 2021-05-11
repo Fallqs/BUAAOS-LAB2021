b4902935dd2fcd35340d1d0084d0354ad6e4c18f
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-11 20:40:10 +0800

    mdzz sbzj

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 const int N=64,M=410,Q=16,R=48;
 int t[64],sz[64],cnt=M;
-inline void pageReplace(long *physic_memery,long nwAdd){
+void pageReplace(long *physic_memery,long nwAdd){
 	register int rnk=nwAdd>>12, j=(rnk&3)<<4;
 	if(sz[j]<Q){
 		register int end=j+sz[j];
