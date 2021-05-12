3f5317d1ad0fa1401112f86c481445f66b5736cb
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-12 10:18:12 +0800

    cache-cache pointer

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -2,30 +2,34 @@
 using namespace std;
 const int N=64,M=410,Q=16,C=256;
 int t[64],sz[4],cnt=M,cache[C]={-1},*ct[C];
-void pageReplace(long *pp,long nwAdd){
+void pageReplace(long *p,long nwAdd){
 	register int rnk(nwAdd>>12), ch=rnk&255;
 	if(cache[ch]==rnk){*ct[ch]=++cnt;return;}
 	register int i(rnk&3), j(i<<4);
 	if(sz[i]<Q){
 		cache[ch]=rnk;
 		register int end(j+sz[i]);
-		for(register long *p=pp+bs,*pend=p+end;p<pend;++p)
-			if(*p==rnk){ *(ct[ch]=t+(p-pp))=++cnt; return;}
+		for(;j<end;++j)
+			if(p[j]==rnk){
+				ct[ch]=t+j;
+				t[j]=++cnt;
+				return;
+			}
 		p[end]=rnk;t[end]=++cnt-M;
 		++sz[i];ct[ch]=t+end;
 		return;
 	}
-	register int *old(t+j);j=*old;
-	register long *p=pp+bs,*pend=p+Q;
-	for(register int *i(old);p<pend;++p,++i){
-		if(*p==rnk){
+	register int old(j);
+	for(register int end(j+Q);j<end;++j){
+		if(p[j]==rnk){
 			cache[ch]=rnk;
-			*(ct[ch]=i)=++cnt;
+			ct[ch]=t+j;
+			t[j]=++cnt;
 			return;
 		}
-		if(*i<j)j=*(old=i);
+		if(t[j]<t[old])old=j;
 	}
-	p=pp+(old-t);
-	cache[*p&255]=0;cache[ch]=rnk;
-	*p=rnk;*old=++cnt-M;ct[ch]=old;
+	cache[p[old]&255]=0;cache[ch]=rnk;
+	p[old]=rnk;t[old]=++cnt-M;ct[ch]=t+old;
 }
+
