c7c10c25ef2f3f9d3e04563d142c32da056cd743
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-12 08:48:04 +0800

    auto

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -4,7 +4,7 @@ const int N=64,M=410,Q=16,C=256;
 int t[64],sz[4],cnt=M,cache[C],*ct[C];
 void pageReplace(long *p,long nwAdd){
 	register int rnk(nwAdd>>12), ch=rnk&255;
-	//if(cache[ch]==rnk){*(ct[ch])=++cnt;return;}
+	if(cache[ch]==rnk){}//*(ct[ch])=++cnt;return;}
 	register int i(rnk&3), j(i<<4);
 	if(sz[i]<Q){
 		cache[ch]=rnk;
@@ -16,19 +16,22 @@ void pageReplace(long *p,long nwAdd){
 				return;
 			}
 		p[end]=rnk;t[end]=++cnt-M;
-		++sz[i];//ct[ch]=t+end;
+		++sz[i];ct[ch]=t+end;
 		return;
 	}
-	register int old(j);
-	for(register int end(j+Q);j<end;++j){
-		if(p[j]==rnk){
-			cache[ch]=rnk;
-			ct[ch]=t+j;
-			t[j]=++cnt;
-			return;
-		}
-		if(t[j]<t[old])old=j;
+register int old(j);
+for(register int end(j+Q);j<end;++j){
+	if(p[j]==rnk){
+		cache[ch]=rnk;
+		ct[ch]=t+j;
+		t[j]=++cnt;
+		return;
 	}
-	//cache[p[old]&255]=0;cache[ch]=rnk;
-	p[old]=rnk;t[old]=++cnt-M;ct[ch]=t+old;
+	if(t[j]<t[old])old=j;
 }
+cache[p[old]&255]=0;cache[ch]=rnk;
+p[old]=rnk;t[old]=++cnt-M;ct[ch]=t+old;
+}
+
+
+
