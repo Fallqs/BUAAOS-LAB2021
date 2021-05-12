51fcc002bdf73c8af3e64c047def25ee0f5587c3
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-12 09:07:05 +0800

    auto

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -1,7 +1,7 @@
 #include "pageReplace.h"
 using namespace std;
 const int N=64,M=410,Q=16,C=256;
-int t[64],sz[4],cnt=M,cache[C],*ct[C];
+int t[64],sz[4],cnt=M,cache[C]={-1},*ct[C];
 void pageReplace(long *p,long nwAdd){
 	register int rnk(nwAdd>>12), ch=rnk&255;
 	if(cache[ch]==rnk){*ct[ch]=++cnt;return;}
@@ -29,7 +29,7 @@ void pageReplace(long *p,long nwAdd){
 		}
 		if(t[j]<t[old])old=j;
 	}
-	cache[p[old]&255]=0;cache[ch]=rnk;
+	cache[p[old]&255]=-1;cache[ch]=rnk;
 	p[old]=rnk;t[old]=++cnt-M;ct[ch]=t+old;
 }
 
