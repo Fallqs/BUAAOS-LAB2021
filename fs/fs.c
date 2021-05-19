4569fad7b0f3ddd12a9b49745f613585553ed8d8
19375273 <jfsilentspring@buaa.edu.cn>
2021-05-19 11:08:18 +0800

    auto lab5

--- fs/fs.c
+++ fs/fs.c
@@ -90,7 +90,7 @@ unmap_block(u_int blockno)
 	u_int addr;
 
 	// Step 1: check if this block is mapped.
-	if(!(addr = block_is_mapped(block)))return;
+	if(!(addr = block_is_mapped(blockno)))return;
 
 	// Step 2: use block_is_free，block_is_dirty to check block , 
 	//if this block is used(not free) and dirty, it needs to be synced to disk: write_block
